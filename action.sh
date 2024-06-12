94956691a7ac7c25fbc7390c1183784e09d329fe
Mark Bestavros <mbestavr@redhat.com>
2024-06-12 13:59:15 -0400

    Make action more reliable
    Signed-off-by: Mark Bestavros <mbestavr@redhat.com>

--- action.sh
+++ action.sh
@@ -1,6 +1,7 @@
 #! /bin/sh -l
 
 REMOTE="${1}"
+GITHUB_TOKEN="${2}"
 
 if [ -z "${REMOTE}" ]; then
   echo Please specify an origin
@@ -8,7 +9,7 @@ if [ -z "${REMOTE}" ]; then
 fi
 
 git config --global --add safe.directory /github/workspace
-git clone --bare "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" . || exit 1
+git clone "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" . || exit 1
 git remote add --mirror=fetch mirror "${REMOTE}" || exit 1
 git fetch mirror +refs/heads/*:refs/remotes/origin/* || exit 1
 git push --force --mirror --prune origin || exit 1
